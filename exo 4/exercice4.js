c71d3b6e61f664901e342066596d65b98ede3716
azxki <cococoursan@gmail.com>
2022-11-23 14:57:33 +0100

    EXO

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,7 +1,10 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
 
+function container(oui) {
+    document.getElementById('container').innerHTML = oui;
+}
 
-
+container('Bonjour');
 /* Appeler la fonction */
 
 
